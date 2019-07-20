d2cd6127d512ce9b4bb0274a8ffba14515079c51
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-07-19 18:11:35 -0700

    Created settings class, setting game's screen dimensions.

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store all settings for Galaga game."""
+
+    def __init__(self):
+        """Initialize game settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
