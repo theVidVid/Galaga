3cd528fcf0c6b37ff319b5258ffd6d7ff6cfceed
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-07-23 00:17:38 -0700

    Added a ship speed factor to 1.5 in settings class. Updated the ship's x position to include float values. Updated moving_left and moving_right set to the ship's x value set from ship_speed setting from settings class.

--- settings.py
+++ settings.py
@@ -6,3 +6,6 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 675
+
+        # Ship settings
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class SpaceShip:
         """Initialize the spaceship's starting position."""
         self.screen = ga_game.screen
         self.screen_rect = ga_game.screen.get_rect()
+        self.settings = ga_game.settings
 
         # Load the ship image and get its rect
         self.image = pygame.image.load('Images/galaga_spaceship.bmp')
@@ -15,6 +16,9 @@ class SpaceShip:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
@@ -22,9 +26,12 @@ class SpaceShip:
     def update(self):
         """Update the ship's moving position based on the movement flag."""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
