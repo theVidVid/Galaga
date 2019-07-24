95421e403cb679e971f910b5101895d79a23d55f
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-07-24 13:44:05 -0700

    Created 4 classes of aliens based on wing color, added height attributes to set positions of each fleet. In GalagaGame class, added new sprite groups for each alien type, created class instance of each alien in _create_alien() helper method, set available horizontal space as done with previous build, and created each row.

--- alien.py
+++ alien.py
@@ -3,7 +3,24 @@ import pygame
 from pygame.sprite import Sprite
 
 
-class Alien(Sprite):
+class GreenWing(Sprite):
+    """Class representing an Alien battleship"""
+
+    def __init__(self, ga_game):
+        """Initialize the alien and set it's starting position."""
+        super().__init__()
+        self.screen = ga_game.screen
+
+        # Load the alien image and set its rect attribute
+        self.image = pygame.image.load('Images/green_enemy.png')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height * 0.05
+
+
+class BlueWing(Sprite):
     """Class representing an Alien battleship"""
     def __init__(self, ga_game):
         """Initialize the alien and set it's starting position."""
@@ -16,7 +33,53 @@ class Alien(Sprite):
 
         # Start each new alien near the top left of the screen.
         self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
+        self.rect.y = self.rect.height * 4
 
-        # Store the alien's exact horizontal position
+        # Store the alien's exact horizontal and vertical position
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+
+class RedWing(Sprite):
+    """Class representing an Alien battleship"""
+    def __init__(self, ga_game):
+        """Initialize the alien and set it's starting position."""
+        super().__init__()
+        self.screen = ga_game.screen
+
+        # Load the alien image and set its rect attribute
+        self.image = pygame.image.load('Images/red_enemy.png')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height * 6.66
+
+        # Store the alien's exact horizontal and vertical position
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+
+class TealWing(Sprite):
+    """Class representing an Alien battleship"""
+
+    def __init__(self, ga_game):
+        """Initialize the alien and set it's starting position."""
+        super().__init__()
+        self.screen = ga_game.screen
+
+        # Load the alien image and set its rect attribute
+        self.image = pygame.image.load('Images/teal_enemy.png')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height * 6
+
+        # Store the alien's exact horizontal and vertical position
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        # Store the alien's exact horizontal and vertical position
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
--- galaga.py
+++ galaga.py
@@ -10,7 +10,7 @@ from ship import SpaceShip
 
 from missile import Missile
 
-from alien import Alien
+from alien import GreenWing, BlueWing, RedWing, TealWing
 
 
 class GalagaGame:
@@ -25,8 +25,10 @@ class GalagaGame:
         pygame.display.set_caption("Galaga")
         self.ship = SpaceShip(self)
         self.missiles = pygame.sprite.Group()
-        self.aliens = pygame.sprite.Group()
-
+        self.green_aliens = pygame.sprite.Group()
+        self.blue_aliens = pygame.sprite.Group()
+        self.red_aliens = pygame.sprite.Group()
+        self.teal_aliens = pygame.sprite.Group()
         self._create_fleet()
         self.background = BackgroundImage(self.settings, self.screen)
 
@@ -76,21 +78,61 @@ class GalagaGame:
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
-        # Make an alien
-        alien = Alien(self)
-        alien_width = alien.rect.width
+        # Make a green, blue, red, and teal wing alien
+        green_alien = GreenWing(self)
+        green_width = green_alien.rect.width
+        green_height = green_alien.rect.height
+        blue_alien = BlueWing(self)
+        blue_width = blue_alien.rect.width
+        blue_height = blue_alien.rect.height
+        red_alien = RedWing(self)
+        red_width = red_alien.rect.width
+        red_height = red_alien.rect.height
+        teal_alien = TealWing(self)
+        teal_width = teal_alien.rect.width
+        teal_height = teal_alien.rect.height
 
         # Find out the available space for an alien fleet
-        available_space_x = self.settings.screen_width - 2 * alien_width
-        number_aliens_x = available_space_x // (1 * alien_width)
+        green_space_x = self.settings.screen_width - 1 * green_width
+        green_number_aliens_x = green_space_x // (1 * green_width)
+        blue_space_x = self.settings.screen_width - 2 * blue_width
+        blue_number_aliens_x = blue_space_x // (1 * blue_width)
+        red_space_x = self.settings.screen_width - 2 * red_width
+        red_number_aliens_x = red_space_x // (1 * red_width)
+        teal_space_x = self.settings.screen_width - 2 * teal_width
+        teal_number_aliens_x = teal_space_x // (1 * teal_width)
 
-        # Create the first row of aliens
-        for alien_number in range(number_aliens_x):
-            # Create an alien and place it in a row
-            alien = Alien(self)
-            alien.x = alien_width + 1.25 * alien_width * alien_number
+        # Create the first row of green wing aliens
+        for alien_number in range(green_number_aliens_x):
+            # Create a green wing alien and place it in a row
+            alien = GreenWing(self)
+            alien.x = green_width + 0.75 * green_width * alien_number
             alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self.green_aliens.add(alien)
+
+        # Create the first row of blue wing aliens
+        for alien_number in range(blue_number_aliens_x):
+            # Create a blue wing alien and place it in a row
+            alien = BlueWing(self)
+            alien.x = blue_width + 1.25 * blue_width * alien_number
+            alien.rect.x = alien.x
+            self.blue_aliens.add(alien)
+
+        # Create the first row of red wing aliens
+        for alien_number in range(red_number_aliens_x):
+            # Create a red wing alien and place it in a row
+            alien = RedWing(self)
+            alien.x = red_width + 1.5 * red_width * (alien_number + 0.35)
+            alien.rect.x = alien.x
+            self.red_aliens.add(alien)
+
+        # Create the first row of teal wing aliens
+        for alien_number in range(teal_number_aliens_x):
+            # Create a blue wing alien and place it in a row
+            alien = TealWing(self)
+            alien.x = teal_width + 1.25 * teal_width * alien_number
+            alien.rect.x = alien.x
+            self.teal_aliens.add(alien)
 
     def _fire_missile(self):
         """Load missile and add it to missiles group."""
@@ -110,7 +152,10 @@ class GalagaGame:
         self.ship.blitme()
         for missile in self.missiles.sprites():
             missile.blitme()
-        self.aliens.draw(self.screen)
+        self.green_aliens.draw(self.screen)
+        self.blue_aliens.draw(self.screen)
+        self.red_aliens.draw(self.screen)
+        self.teal_aliens.draw(self.screen)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
